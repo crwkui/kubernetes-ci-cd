82c6bddd35aa23f94140e0ab1a96a158e6d60b9f
c'rwkui <crwkui@hotmail.com>
2020-06-18 18:05:14 +0800

    12

--- scripts/kr8sswordz-pages.sh
+++ scripts/kr8sswordz-pages.sh
@@ -4,10 +4,10 @@
 BUILD_TAG=`git rev-parse --short HEAD`
 
 #Build the docker image
-docker build -t 192.168.50.39:32772/kr8sswordz:$BUILD_TAG -f applications/kr8sswordz-pages/Dockerfile applications/kr8sswordz-pages
+docker build -t 192.168.50.39:32773/kr8sswordz:$BUILD_TAG -f applications/kr8sswordz-pages/Dockerfile applications/kr8sswordz-pages
 
 #Push the images
-docker push 192.168.50.39:32772/kr8sswordz:$BUILD_TAG
+docker push 192.168.50.39:32773/kr8sswordz:$BUILD_TAG
 
 # Create the deployment and service for the front end aka kr8sswordz
 sed 's#192.168.50.39:32772/kr8sswordz:$BUILD_TAG#192.168.50.39:32772/kr8sswordz:'$BUILD_TAG'#' applications/kr8sswordz-pages/k8s/deployment.yaml | kubectl apply -f -
--- scripts/puzzle.sh
+++ scripts/puzzle.sh
@@ -4,10 +4,10 @@
 BUILD_TAG=`git rev-parse --short HEAD`
 
 #Build the docker image
-docker build -t 192.168.50.39:32772/puzzle:$BUILD_TAG -f applications/puzzle/Dockerfile applications/puzzle
+docker build -t 192.168.50.39:32773/puzzle:$BUILD_TAG -f applications/puzzle/Dockerfile applications/puzzle
 
 #Push the images
-docker push 192.168.50.39:32772/puzzle:$BUILD_TAG
+docker push 192.168.50.39:32773/puzzle:$BUILD_TAG
 
 # Create the deployment and service for the puzzle server aka puzzle
 sed 's#192.168.50.39:32772/puzzle:$BUILD_TAG#192.168.50.39:32772/puzzle:'$BUILD_TAG'#' applications/puzzle/k8s/deployment.yaml | kubectl apply -f -
